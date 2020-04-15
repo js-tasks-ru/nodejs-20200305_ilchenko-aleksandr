fe8e40200a3c316d713f065eaf74f6a8b9d74e70
ILCHENKO Aleksandr <Aleksandr.Ilchenko@vaz.ru>
2020-04-15 11:49:05 +0400

    add solution for 4-module 3-task

--- 4-module/3-task/server.js
+++ 4-module/3-task/server.js
@@ -1,19 +1,36 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
 server.on('request', (req, res) => {
   const pathname = url.parse(req.url).pathname.slice(1);
-
   const filepath = path.join(__dirname, 'files', pathname);
 
   switch (req.method) {
     case 'DELETE':
+      if (pathname.indexOf('\/') >= 0) {
+        res.statusCode = 400;
+        res.end('Bad Request');
+      } else {
+        fs.unlink(filepath, (err) => {
+          if (!err) {
+            return res.end('ok');
+          }
+
+          if (err.code === 'ENOENT') {
+            res.statusCode = 404;
+            res.end('Not Found');
+          } else {
+            res.statusCode = 500;
+            res.end('Internal Server Error');
+          }
+        });
+      }
 
       break;
-
     default:
       res.statusCode = 501;
       res.end('Not implemented');
